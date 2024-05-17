291e7cbbb9733c13c7f58d05b601edfa16f4c5c6
Александр Осипов <alikxyzet@gmail.com>
2024-05-18 00:12:47 +0300

    13.0 Base

---
+++ 0. Testing.cpp
@@ -0,0 +1,65 @@
+/* ---   Проверка патернов через консоль   --- */
+#include "0. Testing.h"
+
+//Выбор по умолчанию
+int DefaultSelection = 1;
+
+bool MainSelection(int sel)
+{
+	switch (sel)
+	{
+	case 1:
+		main1();
+		break;
+	case 2:
+		main2();
+		break;
+	case 3:
+		main3();
+		break;
+	case 4:
+		main4();
+		break;
+	case 5:
+		main5();
+		break;
+	case 6:
+		main6();
+		break;
+	default:
+		std::cout << "Некорректный ввод. Выбор по умолчанию : "
+			<< DefaultSelection
+			<< '\n'
+			<< std::endl;;
+		return true;
+		break;
+	}
+
+	return false;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	std::cout << "Структурные патерны:\n"
+		<< "1. Фасад\n"
+		<< "2. Заместитель\n"
+		<< "3. Компоновщик\n"
+		<< "4. Адаптер\n"
+		<< "5. Мост\n"
+		<< "6. Легковес\n"
+		<< '\n';
+
+	std::cout << "Выбирите тестируемый патерн: ";
+	int Selection;
+	std::cin >> Selection;
+	std::cout << std::endl;
+
+	DefaultSelection = ((DefaultSelection - 1) % 6) + 1;
+
+	if (MainSelection(Selection))
+		MainSelection(DefaultSelection);
+
+	return 0;
+}
---
+++ 0. Testing.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+int main();
+int main1();
+int main2();
+int main3();
+int main4();
+int main5();
+int main6();
